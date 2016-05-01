dbece60598ba226458c73e08dd8e272db07a918c
silent-hero <p.jazauskas@gmail.com>
2016-05-01 13:02:17 +0300

    config suggestions

--- server/config.php
+++ server/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$_hostname = "localhost";
-$_username = "lichat";
-$_password = "wolf";
-$_database = "lichat";
+$_hostname = "your_database_url";
+$_username = "your_database_username";
+$_password = "your_database_password";
+$_database = "your_database_name";
 
 date_default_timezone_set('Europe/Vilnius');
 error_reporting(0);
