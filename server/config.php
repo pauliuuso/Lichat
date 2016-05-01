3fddac6a9ed44837868a80040f366baa4dc9603c
silent-hero <p.jazauskas@gmail.com>
2016-05-01 18:48:38 +0300

    config example

---
+++ server/config.php
@@ -0,0 +1,8 @@
+<?php
+$_hostname = "your_database_url";
+$_username = "your_database_username";
+$_password = "your_database_password";
+$_database = "your_database_name";
+
+date_default_timezone_set('Europe/Vilnius');
+error_reporting(0);
